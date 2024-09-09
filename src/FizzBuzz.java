8dabd113da3cd37f9f908316325fbdf8e7fc07eb
KimberlyFU <kimberlyfu006@gmail.com>
2024-09-09 16:53:36 -0400

    task 3.3

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,7 +1,6 @@
 /**
  * Solve the FizzBuzz challenge.
  */
-// check check check
 //class FizzBuzz {
 //
 //    public static void main(String[] args) {
@@ -40,16 +39,15 @@
  */
 class FizzBuzzWhile {
 
-    public static void main(String[] args) {
+    public static void main() {
 
-        //for (int i = 1; i < 100; i++) {
         int i = 1;
         while (i<100){
             i = doFizzBuzz(i);
         }
     }
 
-    private static int doFizzBuzz(int i) {
+    static int doFizzBuzz(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        //FizzBuzzWhile.doFizzBuzz(5);
+        FizzBuzzWhile.main();
+    }
+}
